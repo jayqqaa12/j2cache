9232d9dbbcd710c48fa1942469bb4c5d812d8b0a
12 <476335667@qq.com>
2017-12-22 15:59:57 +0800

    spring boot

--- src/main/java/com/jayqqaa12/j2cache/spring/aspect/LockAspect.java
+++ src/main/java/com/jayqqaa12/j2cache/spring/aspect/LockAspect.java
@@ -10,6 +10,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,7 +18,7 @@ import org.springframework.stereotype.Service;
  */
 @Aspect
 @Service
-public class LockAspect {
+public class LockAspect implements Ordered {
 
     public static final String  LOCK="LOCK:";
 
@@ -52,4 +53,8 @@ public class LockAspect {
         throw new LockException("get lock fail!!!  ");
     }
 
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
